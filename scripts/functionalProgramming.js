cc34f74c1c38ac2840469e98b7f1767e38d4c117
nguyendangtu <nguyendangtu1987@gmail.com>
2023-01-23 21:26:07 -0600

    update functionalProgramming

--- scripts/functionalProgramming.js
+++ scripts/functionalProgramming.js
@@ -100,25 +100,3 @@
 
             console.log("Expected output of filterLongWords([John, David, Tom], 4) is 'David'  " + myFunctionTest("David",filterLongWords(["John", "David", "Tom"],4)[0]));
             console.log("Expected output of filterLongWords([John, David, Tom], 5) is 'undefined'  " + myFunctionTest(undefined,filterLongWords(["John", "David", "Tom"],5)[0]));
-           
-            
-            const a = [1,3,5,3,3]; 
-            const b = a.map(function(elem, i, array) {
-                return elem * 10;
-            })
-            document.writeln(b.toString() + "<br/>");
-
-            const c = a.filter(function(elem, i, array){
-                return elem == 3;});
-            document.writeln(c.toString() + "<br/>");
-
-
-            const d = a.reduce(function(prevValue, elem, i, array){
-                return ""+prevValue + elem;
-            });
-            document.writeln(d+ "<br/>");
-
-            const d2 = a.find(function(elem) {return elem > 1;}); //3
-            const d3 = a.findIndex(function(elem) {return elem > 1;}); //1
-            document.writeln(d2+ "<br/>");
-            document.writeln(d3+ "<br/>");
