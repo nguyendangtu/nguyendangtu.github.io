57d98f799213741c44d9250d89504c99e9e02208
nguyendangtu <nguyendangtu1987@gmail.com>
2023-02-07 20:31:00 -0600

    update

--- nodejs/w4d1/q3_4/index.js
+++ nodejs/w4d1/q3_4/index.js
@@ -19,7 +19,35 @@ app.set('views', path.join(__dirname, 'views'));
 
 app.use('/', indexRouter);
 
+app.get('/product',(req,res)=>{
+    let index = parseInt(req.query.index);
+    res.render('product', {
+        product: inventory[index]
+    });
+});
 
+app.post('/cart', (req,res)=>{
+    const name = req.body.p;
+    let product = inventory.filter(x=>x.name.split(" ")[0] == name)[0];
+    let cart = [];
+    if(req.session.cart){
+        cart = req.session.cart;
+    }
+    cart.push(product);
+    req.session.cart = cart;
+    res.redirect('/cart');
+});
+
+
+app.get('/cart', (req,res) => {
+    let cart = [];
+    if(req.session.cart){
+        cart = req.session.cart;
+    }
+    res.render('cart',{
+        cart: cart
+    })
+});
 
 app.listen(3000, ()=>console.log("Server is running...."));
 module.exports = app;
